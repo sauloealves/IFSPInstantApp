b66ba4fc205fc96afd0a59bcb3ec82a9c037ed93
sauloealves <sauloealves@gmail.com>
2019-09-05 13:51:46 -0300

    Initial commit

---
+++ base/src/main/java/sauloealves/com/ifspinstantapp/MainActivity.java
@@ -0,0 +1,16 @@
+package sauloealves.com.ifspinstantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sauloealves.com.ifspinstantapp.base.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
